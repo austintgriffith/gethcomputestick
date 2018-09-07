e7ce609fbd0f3f76beb7f8ebbfe130f8d60b0993
Austin Griffith <me@austingriffith.com>
2018-09-07 12:19:27 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/geth --ipcpath "/root/geth.ipc" --datadir "gethdata" --cache 256 --maxpeers 50 --light --rpc --rpcapi="db,eth,net,web3,personal" --rpccorsdomain="*"
+/usr/bin/geth --ipcpath "/root/geth.ipc" --datadir "gethdata" --cache 256 --maxpeers 50 --syncmode "light" --rpc --rpcapi="db,eth,net,web3,personal" --rpccorsdomain="*"
